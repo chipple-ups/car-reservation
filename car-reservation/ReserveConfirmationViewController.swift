dff500204a0aa8fcfe3df4ed2b45dd9465d8c0af
hiroshi.okazawa <chipple_ups@yahoo.co.jp>
2021-05-24 21:59:23 +0900

    ２０２１０５２４

--- car-reservation/ReserveConfirmationViewController.swift
+++ car-reservation/ReserveConfirmationViewController.swift
@@ -6,12 +6,34 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseUI
 
 class ReserveConfirmationViewController: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var nocLabel: UILabel!
+    @IBOutlet weak var itemLabel: UILabel!
+    
+    var reserveusedate : String?
+    var selectedplace : String?
+    var usenoc : Int?
+    var itemText : String?
+    var userNameText : String?
+    var selectednoc : Int?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        dateLabel.text = reserveusedate
+        placeLabel.text = selectedplace
+        //nocLabel.text = String(usenoc!)
+        itemLabel.text = itemText
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
     
--- car-reservation/ReserveViewController.swift
+++ car-reservation/ReserveViewController.swift
@@ -8,21 +8,106 @@
 import UIKit
 import Firebase
 
-class ReserveViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ReserveViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate{
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textItem: UITextField!
+    @IBOutlet weak var textUserName: UITextField!
+    @IBOutlet weak var textNoc: UITextField!
+    @IBOutlet weak var reserveButton: UIButton!
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    private var nocText: Any?
+    private var itemText: String?
+    private var userNameText: String?
     
     var placelistArray: [[String:Any]] = []
     var listener: ListenerRegistration?
     var placelistCount: Int = 0
     var selectPlaceList: Int = 0
-    
+    var setreservetextItem: String = ""
+    var tableViewSelected: Int = 0
+    var selctreserveDate: String?
+    var selectedplace: String = ""
+    var selectednoc: Int = 0
+    var usenoc: Int = 0
+    var reserveusedate: Date = Date()
 
+    @IBAction func nocEditChanged(_ sender: UITextField) {
+        self.nocText = sender.text ?? 0
+        self.usenoc = Int(sender.text!) ?? 0
+        self.validate()
+    }
+    
+    @IBAction func itemEditChanged(_ sender: UITextField) {
+        self.itemText = sender.text
+        self.validate()
+    }
+    
+    @IBAction func userNameEditChanged(_ sender: UITextField) {
+        self.userNameText = sender.text
+        self.validate()
+    }
+    
+    private func validate() {
+        
+        
+        let noc = selectednoc - usenoc
+        
+        guard let nocText = self.nocText as? String,
+              tableViewSelected == 1 ,
+               let itemText = self.itemText,
+               let userNameText = self.userNameText else {
+                 
+                 self.reserveButton.isEnabled = false
+                   return
+        }
+        
+        if nocText.count == 0 || noc < 0  || itemText.count == 0 || userNameText.count == 0 {
+               self.reserveButton.isEnabled = false
+               return
+           }
+           
+           self.reserveButton.isEnabled = true
+    }
+    
+    @IBAction func reservecheckButton(_ sender: Any) {
+        print(nocText!)
+        print(itemText!)
+        print(userNameText!)
+        print(selctreserveDate!)
+        print(selectedplace)
+        print(selectednoc)
+        let noc = selectednoc - usenoc
+        print("使用台数\(noc)")
+        print("予約日\(reserveusedate)")
+        
+        let reserveConfirmationViewController = storyboard!.instantiateViewController(withIdentifier: "ReserveConfirmation") as! ReserveConfirmationViewController
+
+        //reserveConfirmationViewController.reserveusedate = selctreserveDate
+        //reserveConfirmationViewController.selectedplace = selectedplace
+        //reserveConfirmationViewController.usenoc = usenoc
+        //reserveConfirmationViewController.itemText = itemText
+        
+            present(reserveConfirmationViewController, animated: true)
+        
+        
+    }
+    
     
     @IBAction func datePicker(_ sender: UIDatePicker) {
         
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyyMMdd"
         let myinputDatePicker = formatter.string(from: sender.date)
+        
+        //let datePicker = DateFormatter()
+        //formatter.dateFormat = "yyyy年MM月dd日"
+        //let useDatePicker = datePicker(sender.date)
+
+        reserveusedate = self.datePicker.date
+        selctreserveDate = myinputDatePicker
+        
         
         print(myinputDatePicker)
     
@@ -60,9 +145,19 @@ class ReserveViewController: UIViewController, UITableViewDelegate, UITableViewD
         
         tableView.delegate = self
         tableView.dataSource = self
+        textItem.delegate = self
+        textUserName.delegate = self
 
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+ 
+        self.setreservetextItem = textField.text!
+        // キーボードを閉じる
+        textField.resignFirstResponder()
+ 
+        return true
+    }
     
     
         // データの数（＝セルの数）を返すメソッド
@@ -102,6 +197,16 @@ class ReserveViewController: UIViewController, UITableViewDelegate, UITableViewD
             let task = placelistArray[indexPath.row]
             for (key, value) in task {
               print("\(key) -> \(value)")
+                
+                selectedplace = task["place"] as! String
+                selectednoc = Int(task["noc"] as! String)!
+                
+                print(selectedplace)
+                print(selectednoc)
+                
+                
+                tableViewSelected = 1
+                self.validate()
                 
                 //self.placedata = task["place"] as? String
                 //self.nocdata = task["noc"]
